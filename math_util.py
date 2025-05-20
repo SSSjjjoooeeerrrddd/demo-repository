6b98472702cf82b2749f4e7c67db7642d3d0265d
SSSjjjoooeeerrrddd <sjoerd.gruson@veneficus.nl>
2025-05-20 09:52:31 +0200

    Added multiply and divide to math_util.py

--- math_util.py
+++ math_util.py
@@ -5,3 +5,15 @@ def add(x, y):
 def substract(x, y):
     """Substracts two numbers and returns the result"""
     return x - y
+
+def multiply(x, y):
+    """Multiplies two numbers and returns the result"""
+    return x * y
+
+def divide(x, y):
+    """Divides two numbers and returns the result
+    Raises ZeroDivisionError if the second number (y) is zero"""
+    if y != 0:
+        return x / y
+    else:
+        raise ZeroDivisionError("Wie deelt door nul is een sul.")
