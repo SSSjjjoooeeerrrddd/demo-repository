7602de4dfa822d3f503f6aa3d4746550eacb2f93
SSSjjjoooeeerrrddd <sjoerd.gruson@veneficus.nl>
2025-05-20 09:48:15 +0200

    Create math_util.py

---
+++ math_util.py
@@ -0,0 +1,7 @@
+def add(x, y):
+    """Adds two numbers and returns the result"""
+    return x + y
+
+def substract(x, y):
+    """Substracts two numbers and returns the result"""
+    return x - y
